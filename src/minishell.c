df756598fdd42322bac675ff1d4a025bfd836b61
Antoine GAUTIER <agautier@student.42.fr>
2021-05-09 17:27:22 +0200

    fix error message

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: agautier <agautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 22:03:32 by agautier          #+#    #+#             */
-/*   Updated: 2021/05/09 17:11:39 by agautier         ###   ########.fr       */
+/*   Updated: 2021/05/09 17:26:59 by agautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ void	minishell(t_list *env, t_err *err)
 			buf[len] = '\n';
 			if(!(lexer(buf, err, env)))
 			{
-				// ft_putendl_fd(err->message[err->code], STDERR_FILENO);	// TODO: etait utile ?
+				ft_putendl_fd(err->message[err->code], STDERR_FILENO);	// TODO: etait utile ?
 				// printf("erreur dans minishell\n");
 				// return (EXIT_FAILURE);
 				// continue ;
